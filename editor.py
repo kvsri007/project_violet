9533da96716e96852600ef60955ee9337fa3a746
kvsri007 <kvsriiram2007@gmail.com>
2025-05-21 16:37:18 +0530

    Update editor.py

--- editor.py
+++ editor.py
@@ -19,24 +19,43 @@ IG_USER_ID = os.getenv("IG_ID")
 REPO_OWNER = "kvsri007"         
 REPO_NAME = "project_violet"                      
 BRANCH = "main"                              
-LOCAL_IMAGE_PATH = "./content/"   
+#LOCAL_IMAGE_PATH = "./content/"   
 REPO_IMAGE_PATH = "content/"         
 direct_links = []
 GITHUB_PATH = "./content/data.json"
 me = "kvsriiram2007@gmail.com"
 
+######
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # Root directory where your script is
+ASSET_DIR = os.path.join(BASE_DIR, "assets")
+CONTENT_DIR = os.path.join(BASE_DIR, "content")
+# Font paths
+FONT_BOLD = os.path.join(ASSET_DIR, "timesbd.ttf")
+FONT_BOLD_ITALIC = os.path.join(ASSET_DIR, "timesbi.ttf")
+FONT_ITALIC = os.path.join(ASSET_DIR, "timesi.ttf")
+
+# Template paths
+TEMPLATE_ONE = os.path.join(ASSET_DIR, "t_one.png")
+TEMPLATE_TWO = os.path.join(ASSET_DIR, "t_two.png")
+TEMPLATE_THREE = os.path.join(ASSET_DIR, "t_three.png")
+
+CONTENT_ONE = os.path.join(CONTENT_DIR, "1.jpg")
+CONTENT_TWO = os.path.join(CONTENT_DIR, "2.jpg")
+CONTENT_THREE = os.path.join(CONTENT_DIR, "3.jpg")
 
 def generate_post(WORD,IPA,DEFINITION,EXAMPLE_1,EXAMPLE_2):
     try:
         print("content sent to editor")
-        gen_page1("./assets/t_one.png",WORD,IPA,"./content/1.jpg")
-        gen_page2("./assets/t_two.png","Definition",DEFINITION,"","./content/2.jpg")
-        gen_page2("./assets/t_three.png","Examples",EXAMPLE_1,EXAMPLE_2,"./content/3.jpg")
+        gen_page1(TEMPLATE_ONE,WORD,IPA,CONTENT_ONE)
+        gen_page2(TEMPLATE_TWO,"Definition",DEFINITION,"",CONTENT_TWO)
+        gen_page2(TEMPLATE_THREE,"Examples",EXAMPLE_1,EXAMPLE_2,CONTENT_THREE)
         print("Generated Succefully ")
         try :
             for i in range(1,4):
                 
-                url = upload.upload_img(GITHUB_TOKEN,REPO_OWNER,REPO_NAME,BRANCH,LOCAL_IMAGE_PATH+str(i)+".jpg",REPO_IMAGE_PATH+WORD+"_"+str(i)+".jpg")
+                url = upload.upload_img(GITHUB_TOKEN,REPO_OWNER,REPO_NAME,BRANCH,os.path.join(CONTENT_DIR,str(i)+".jpg"),REPO_IMAGE_PATH+WORD+"_"+str(i)+".jpg")
                 direct_links.append(url)
                 time.sleep(1)
 
@@ -53,5 +72,3 @@ def generate_post(WORD,IPA,DEFINITION,EXAMPLE_1,EXAMPLE_2):
 
     except Exception as e:
         send_mail.mail(me,me,"Error - editor.py",str(e))
-
-
